a4c45f16d645505b57970653ddd61b99eefdb56a
igoretk <balalayka2002@mail.ru>
2016-12-03 11:45:07 +0200

    задание 11 - добавлен тест testTask11NewCustomerReg

--- firststeps/src/test/java/ru/testing/hometasks/tests/litecart/app/TestsLitecartApp.java
+++ firststeps/src/test/java/ru/testing/hometasks/tests/litecart/app/TestsLitecartApp.java
@@ -215,6 +215,69 @@ public class TestsLitecartApp extends TestBase {
       }
     }
   }
+  @Test
+  public void testTask11NewCustomerReg() {
+    driver.navigate().to("http://localhost/litecart/public_html/en/");
+    driver.findElement(By.xpath(".//*[@id='box-account-login']//tr[5]/td/a")).click();
+
+    WebElement taxId = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[1]//td[1]/input"));
+    taxId.clear();
+    taxId.sendKeys("111222333");
+
+    WebElement company = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[1]//td[2]/input"));
+    company.clear();
+    company.sendKeys("test_Company");
+
+    WebElement firstName = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[2]//td[1]/input"));
+    firstName.clear();
+    firstName.sendKeys("Vasisualiy");
+
+    WebElement lastName = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[2]//td[2]/input"));
+    lastName.clear();
+    lastName.sendKeys("Lohankin");
+
+    WebElement adr1 = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[3]//td[1]/input"));
+    adr1.clear();
+    adr1.sendKeys("test_Address 1");
+
+    WebElement adr2 = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[3]//td[2]/input"));
+    adr2.clear();
+    adr2.sendKeys("test_Address 2");
+
+    WebElement postCode = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[4]//td[1]/input"));
+    postCode.clear();
+    postCode.sendKeys("61174");
+
+    WebElement city = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[4]//td[2]/input"));
+    city.clear();
+    city.sendKeys("61174");
+
+    WebElement countryToSelect = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[5]//td[1]/select"));
+    Select selectCountry = new Select(countryToSelect);
+    selectCountry.selectByVisibleText("United States");
+
+    Select selectState = new Select(driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[5]//td[2]/select")));
+    selectState.selectByVisibleText("Alabama");
+
+    WebElement email = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[6]//td[1]/input"));
+    email.clear();
+    email.sendKeys("VasisualiyLohankin@sermjagnaya.pr");
+
+    WebElement phone = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[6]//td[2]/input"));
+    phone.clear();
+    phone.sendKeys("+38050654321");
+
+    WebElement desiredPsw = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[8]//td[1]/input"));
+    desiredPsw.clear();
+    desiredPsw.sendKeys("passwordQwe123");
+
+    WebElement coonfirmPsw = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[8]//td[2]/input"));
+    coonfirmPsw.clear();
+    coonfirmPsw.sendKeys("passwordQwe123");
+
+    WebElement buttonCreateAcc = driver.findElement(By.xpath(".//*[@id='create-account']//table//tr[9]//td[1]/button"));
+    buttonCreateAcc.click();
+  }
 }
 
 
